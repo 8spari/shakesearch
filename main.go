a28c5bbb5df08b79f08422442324e0cf6ef00775
Sangavi Pari <sangavipari@Sangavis-MacBook-Air.local>
2023-04-22 23:55:26 -0700

    Changes to app.js and main.go

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"sort"
+	"strings"
 )
 
 func main() {
@@ -36,8 +38,10 @@ func main() {
 }
 
 type Searcher struct {
-	CompleteWorks string
-	SuffixArray   *suffixarray.Index
+	CompleteWorks     string
+	SuffixArray       *suffixarray.Index
+	ChapterMap        map[int]string
+	ChapterIndexArray []int
 }
 
 func handleSearch(searcher Searcher) func(w http.ResponseWriter, r *http.Request) {
@@ -69,14 +73,116 @@ func (s *Searcher) Load(filename string) error {
 	}
 	s.CompleteWorks = string(dat)
 	s.SuffixArray = suffixarray.New(dat)
+	s.organizeTableOfContents()
 	return nil
 }
 
-func (s *Searcher) Search(query string) []string {
-	idxs := s.SuffixArray.Lookup([]byte(query), -1)
-	results := []string{}
+func (s *Searcher) organizeTableOfContents() {
+	results := s.SearchContents()
+	idx := results[0]
+	contentString := s.CompleteWorks[idx : idx+1970]
+
+	s.getChapters(contentString)
+}
+
+func (s *Searcher) SearchContents() []int {
+	idxs := s.SuffixArray.Lookup([]byte("Contents"), -1)
+	return idxs
+}
+
+func (s *Searcher) getChapters(contents string) {
+
+	titles := strings.Split(contents, "\n")
+	tempMap := make(map[int]string)
+	tempArray := []int{}
+	for _, title := range titles {
+		title = strings.TrimSpace(title)
+
+		if title != "" && title != "Contents" {
+			index := 1
+			if title == "THE TRAGEDY OF ANTONY AND CLEOPATRA" {
+				title = "ANTONY AND CLEOPATRA"
+			} else if title == "THE LIFE OF KING HENRY THE FIFTH" {
+				title = "THE LIFE OF KING HENRY V"
+				index = 0
+			} else if title == "THE TRAGEDY OF MACBETH" {
+				title = "MACBETH"
+			} else if title == "THE TRAGEDY OF OTHELLO, MOOR OF VENICE" {
+				title = "OTHELLO, THE MOOR OF VENICE"
+				index = 0
+			} else if title == "TWELFTH NIGHT; OR, WHAT YOU WILL" {
+				title = "TWELFTH NIGHT: OR, WHAT YOU WILL"
+				index = 0
+			}
+			curr := s.getChapter(title, index)
+			tempMap[curr] = title
+			tempArray = append(tempArray, curr)
+		}
+	}
+	s.ChapterMap = tempMap
+	sort.Ints(tempArray)
+	s.ChapterIndexArray = tempArray
+}
+
+func (s *Searcher) getChapter(title string, index int) int {
+	idxs := s.SuffixArray.Lookup([]byte(title), -1)
+	sort.Ints(idxs)
+	return idxs[index]
+}
+
+func (s *Searcher) binarySearch(needle int) string {
+
+	low := 0
+	high := len(s.ChapterIndexArray) - 1
+
+	for low <= high {
+		median := (low + high) / 2
+
+		if s.ChapterIndexArray[median] < needle {
+			low = median + 1
+		} else {
+			high = median - 1
+		}
+	}
+
+	if low == 0 {
+		return "Pre-chapter"
+	}
+	index := s.ChapterIndexArray[low-1]
+	return s.ChapterMap[index]
+}
+
+func (s *Searcher) getAllIdxs(query string) []int {
+	query_uppercase := strings.ToUpper(query)
+	query_lowercase := strings.ToLower(query)
+	query_capitalize := strings.Title(query_lowercase)
+
+	idxs := s.SuffixArray.Lookup([]byte(query_lowercase), -1)
+	upperIdxs := s.SuffixArray.Lookup([]byte(query_uppercase), -1)
+	capitalIdxs := s.SuffixArray.Lookup([]byte(query_capitalize), -1)
+	idxs = append(idxs, upperIdxs...)
+	idxs = append(idxs, capitalIdxs...)
+	sort.Ints(idxs)
+
+	return idxs
+}
+
+func (s *Searcher) Search(query string) [][]string {
+
+	idxs := s.getAllIdxs(query)
+
+	results := [][]string{}
+
 	for _, idx := range idxs {
-		results = append(results, s.CompleteWorks[idx-250:idx+250])
+		_, ok := s.ChapterMap[idx] //is the actual contents title
+		if ok || idx < s.ChapterIndexArray[0] {
+			continue
+		}
+
+		currExcerpt := s.CompleteWorks[idx-250 : idx+250]
+		chapter := s.binarySearch(idx)
+		row := []string{chapter, currExcerpt}
+		results = append(results, row)
 	}
 	return results
 }
--- static/app.js
+++ static/app.js
@@ -5,19 +5,91 @@ const Controller = {
     const data = Object.fromEntries(new FormData(form));
     const response = fetch(`/search?q=${data.query}`).then((response) => {
       response.json().then((results) => {
-        Controller.updateTable(results);
+        Controller.updateTable(results, data.query);
       });
     });
   },
 
-  updateTable: (results) => {
+  updateTable: (results, query) => {
     const table = document.getElementById("table-body");
+    const tableHeader = document.getElementById("table-header");
     const rows = [];
+    rows.push(`<tr>
+      <th scope="col">Chapter</th>
+      <th scope="col", style="text-align:left">Excerpt</th>
+    </tr>`)
     for (let result of results) {
-      rows.push(`<tr>${result}<tr/>`);
+      [chapter, resString] = result
+      rows.push(`<tr>
+        <th scope="row", style="vertical-align:top">${chapter}</th>
+        <td>${Controller.updateResult(resString, query)}</td>
+      </tr>`)
     }
     table.innerHTML = rows;
   },
+
+  updateResult: (result, query) => {
+    result = Controller.formatResultString(result)
+    queryLower = query.toLowerCase();
+    queryUpper = query.toUpperCase();
+    queryCapitalize = queryLower.charAt(0).toUpperCase() + queryLower.slice(1);
+    result = Controller.highlightSearchTerm(result, queryLower)
+    result = Controller.highlightSearchTerm(result, queryUpper)
+    result = Controller.highlightSearchTerm(result, queryCapitalize)
+
+    return result
+
+  },
+
+  checkForRomanNumeral: (text) => {
+    romanNumeralRegex = /^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3}[.]?)$/
+    match = text.match(romanNumeralRegex);
+    if (match == null){ return false }
+    return true
+  },
+
+  createSeparateLine: (text) => {
+    return '<p>' + text + '<\p>';
+  },
+
+
+
+  formatResultString: (result) => {
+    allCapLettersRegex = /(\b[A-Z]{2,}\b[,.]?)/
+    const newText = result.split(allCapLettersRegex)
+
+    newResult = ""
+    i = 0
+    while(i < newText.length){
+      if (newText[i] == 'ACT' || newText[i] == 'SCENE' ){
+        if (i + 2 < newText.length && Controller.checkForRomanNumeral(newText[i+1])){
+          newResult += Controller.createSeparateLine(`${newText[i]} ${newText[i+1]} ${newText[i+2]}`);
+          i = i + 3
+          continue
+        }
+        if (i + 1 < newText.length) {
+          newResult += Controller.createSeparateLine(`${newText[i]} ${newText[i+1]}`);
+          i = i + 2
+          continue
+        }
+      }
+
+      if (i + 1 < newText.length && Controller.checkForRomanNumeral(newText[i+1])){
+        newResult += Controller.createSeparateLine(`${newText[i]} ${newText[i+1]}`);
+        i = i + 2
+        continue
+      }
+
+      newResult += '<p>' + newText[i] + '<\p>';
+      i++
+    }
+
+    return newResult
+  },
+
+  highlightSearchTerm: (result, query) => {
+    return result.replaceAll(query, '<span style="background-color:#ffbf00;color:#fff;"><b>' + query + '</b></span>')
+  }
 };
 
 const form = document.getElementById("form");
